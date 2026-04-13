4fa4e6298529844f59494e856d34b7a2a7ce7976
elviraKarnak <karnak@elvirainfotech.com>
2026-04-13 12:39:55 +0530

    helper-script-0.0.0.5

--- wp-content/themes/usb-v3/assets/js/new-script.js
+++ wp-content/themes/usb-v3/assets/js/new-script.js
@@ -16,74 +16,74 @@ function usbSaveProductSummary(productID) {
     });
 }
 
-// jQuery(document).on('click', '.down_pdf_summary', function (e) {
-//     e.preventDefault();
+jQuery(document).on('click', '.down_pdf_summary', function (e) {
+    e.preventDefault();
 
-//     var $link = jQuery(this);
-//     var productID = $link.data('productid');
-//     var pdfUrl = $link.attr('href');
-//     var target = $link.attr('target');
-//     var newWindow = null;
+    var $link = jQuery(this);
+    var productID = $link.data('productid');
+    var pdfUrl = $link.attr('href');
+    var target = $link.attr('target');
+    var newWindow = null;
 
-//     if (target === '_blank') {
-//         newWindow = window.open('', '_blank');
-//     }
+    if (target === '_blank') {
+        newWindow = window.open('', '_blank');
+    }
 
-//     usbSaveProductSummary(productID)
-//         .done(function (res) {
-//             console.log('pdf summary save success:', res);
-//             console.log('redirect pdf url:', pdfUrl);
+    usbSaveProductSummary(productID)
+        .done(function (res) {
+            console.log('pdf summary save success:', res);
+            console.log('redirect pdf url:', pdfUrl);
 
-//             if (newWindow) {
-//                 newWindow.location = pdfUrl;
-//             } else {
-//                 window.location.href = pdfUrl;
-//             }
-//         })
-//         .fail(function (xhr, status, error) {
-//             console.log('pdf summary save failed:', status, error);
-//             console.log(xhr.responseText);
+            if (newWindow) {
+                newWindow.location = pdfUrl;
+            } else {
+                window.location.href = pdfUrl;
+            }
+        })
+        .fail(function (xhr, status, error) {
+            console.log('pdf summary save failed:', status, error);
+            console.log(xhr.responseText);
 
-//             if (newWindow) {
-//                 newWindow.close();
-//             }
-//         });
-// });
+            if (newWindow) {
+                newWindow.close();
+            }
+        });
+});
 
-// jQuery(document).on('click', '.down_pdf_summary', function (e) {
-//     e.preventDefault();
+jQuery(document).on('click', '.down_pdf_summary', function (e) {
+    e.preventDefault();
 
-//     var $link = jQuery(this);
-//     var productID = $link.data('productid');
-//     var pdfUrl = $link.attr('href');
+    var $link = jQuery(this);
+    var productID = $link.data('productid');
+    var pdfUrl = $link.attr('href');
 
-//     usbSaveProductSummary(productID)
-//         .done(function (res) {
-//             console.log('pdf summary save success:', res);
-//             console.log('redirect pdf url:', pdfUrl);
+    usbSaveProductSummary(productID)
+        .done(function (res) {
+            console.log('pdf summary save success:', res);
+            console.log('redirect pdf url:', pdfUrl);
 
-//             window.location.href = pdfUrl;
-//         })
-//         .fail(function (xhr, status, error) {
-//             console.log('pdf summary save failed:', status, error);
-//             console.log(xhr.responseText);
-//         });
-// });
+            window.location.href = pdfUrl;
+        })
+        .fail(function (xhr, status, error) {
+            console.log('pdf summary save failed:', status, error);
+            console.log(xhr.responseText);
+        });
+});
 
 
-// jQuery(document).on('click', '.optionclick', function () {
-//     setTimeout(function () {
-//         var productID = jQuery(".down_pdf_summary").attr("data-productid");
-//         usbSaveProductSummary(productID)
-//             .done(function (res) {
-//                 console.log('option summary save success:', res);
-//             })
-//             .fail(function (xhr, status, error) {
-//                 console.log('option summary save failed:', status, error);
-//                 console.log(xhr.responseText);
-//             });
-//     }, 1000);
-// });
+jQuery(document).on('click', '.optionclick', function () {
+    setTimeout(function () {
+        var productID = jQuery(".down_pdf_summary").attr("data-productid");
+        usbSaveProductSummary(productID)
+            .done(function (res) {
+                console.log('option summary save success:', res);
+            })
+            .fail(function (xhr, status, error) {
+                console.log('option summary save failed:', status, error);
+                console.log(xhr.responseText);
+            });
+    }, 1000);
+});
 
 
    jQuery(document).on('click', '.down_pdf_summary:not(.marginpdf)', function (e) {

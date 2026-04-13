83e1c7d5d1c9dfc1522451e7115edff8de72e65a
elviraKarnak <karnak@elvirainfotech.com>
2026-04-13 12:55:43 +0530

    helper-script-0.0.0.10

--- wp-content/themes/usb-v3/assets/js/new-script.js
+++ wp-content/themes/usb-v3/assets/js/new-script.js
@@ -16,61 +16,6 @@ function usbSaveProductSummary(productID) {
     });
 }
 
-jQuery(document).on('click', '.down_pdf_summary', function (e) {
-    e.preventDefault();
-
-    var $link = jQuery(this);
-    var productID = $link.data('productid');
-    var pdfUrl = $link.attr('href');
-    var target = $link.attr('target');
-    var newWindow = null;
-
-    // if (target === '_blank') {
-    //     newWindow = window.open('', '_blank');
-    // }
-
-    usbSaveProductSummary(productID)
-        .done(function (res) {
-            console.log('pdf summary save success:', res);
-            console.log('redirect pdf url:', pdfUrl);
-
-            if (newWindow) {
-                newWindow.location = pdfUrl;
-            } else {
-                window.location.href = pdfUrl;
-            }
-        })
-        .fail(function (xhr, status, error) {
-            console.log('pdf summary save failed:', status, error);
-            console.log(xhr.responseText);
-
-            if (newWindow) {
-                newWindow.close();
-            }
-        });
-});
-
-jQuery(document).on('click', '.down_pdf_summary.marginpdf', function (e) {
-    e.preventDefault();
-
-    var $link = jQuery(this);
-    var productID = $link.data('productid');
-    var pdfUrl = $link.attr('href');
-
-    usbSaveProductSummary(productID)
-        .done(function (res) {
-            console.log('pdf summary save success:', res);
-            console.log('redirect pdf url:', pdfUrl);
-
-            window.location.href = pdfUrl;
-        })
-        .fail(function (xhr, status, error) {
-            console.log('pdf summary save failed:', status, error);
-            console.log(xhr.responseText);
-        });
-});
-
-
 // jQuery(document).on('click', '.optionclick', function () {
 //     setTimeout(function () {
 //         var productID = jQuery(".down_pdf_summary").attr("data-productid");
